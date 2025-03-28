cd04684c3573a1b959a3044c8e451a08b41b310c
Lee ChangRyoel <dlckdfuf141@naver.com>
2025-03-28 21:49:12 +0900

    Add files via upload
    수정 삭제 추가

--- demo/src/main/java/com/example/demo/free/FreeController.java
+++ demo/src/main/java/com/example/demo/free/FreeController.java
@@ -6,9 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 
 //생성자를 자동으로 생성해줌(final, not null, 필드들을 매개변수로 사용하는 생성자 자동생성), Lombok의 기능
@@ -22,6 +20,8 @@ public class FreeController {
     public String list(HttpSession session, Model model){
         SiteUser loggedInUser = (SiteUser) session.getAttribute("loggedInUser");
         model.addAttribute("loggedInUser", loggedInUser);
+
+        model.addAttribute("posts", freeService.getAllPosts());
         return "/free/list";
     }
 
@@ -40,6 +40,41 @@ public class FreeController {
         if(loggedInUser == null)
             return "redirect:/user/login";
         freeService.createPost(title, content);
-        return "/free/list";
+        return "redirect:/free/list";
+    }
+
+    @GetMapping("/free/detail/{id}")
+    public String detail(@PathVariable("id") Long id, Model model)
+    {
+        model.addAttribute("post", freeService.getPostId(id));
+        return "/free/detail";
+    }
+
+    //수정
+    @GetMapping("/free/modify/{id}")
+    public String modify(@PathVariable("id") Long id, Model model, HttpSession session){
+        Free post = freeService.getPostId(id);
+        model.addAttribute("post", post);
+        SiteUser loggedInUser = (SiteUser) session.getAttribute("loggedInUser");
+        if(loggedInUser == null)
+            return "redirect:/user/login";
+        return "/free/modify";
+    }
+
+    @PostMapping("/free/modify/{id}")
+    public String modify(@PathVariable("id") Long id, @RequestParam String title, @RequestParam String content, HttpSession session)
+    {
+        SiteUser loggedInUser = (SiteUser) session.getAttribute("loggedInUser");
+        if(loggedInUser == null)
+            return "redirect:/user/login";
+
+        freeService.modifyPost(id, title, content);
+        return "redirect:/free/detail/" + id;
+    }
+
+    @GetMapping("/free/delete/{id}")
+    public String delete(@PathVariable("id") Long id){
+        freeService.deletePost(id);
+        return "redirect:/free/list";
     }
 }
--- demo/src/main/java/com/example/demo/free/FreeService.java
+++ demo/src/main/java/com/example/demo/free/FreeService.java
@@ -4,6 +4,8 @@ import com.example.demo.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 //생성자를 자동으로 생성해줌(final, not null, 필드들을 매개변수로 사용하는 생성자 자동생성), Lombok의 기능
 @RequiredArgsConstructor
 @Service
@@ -17,4 +19,24 @@ public class FreeService {
         freeRepository.save(post);
     }
 
+    public void modifyPost(Long id, String title, String content)
+    {
+        Free post = getPostId(id);
+        post.setTitle(title);
+        post.setContent(content);
+        freeRepository.save(post);
+    }
+    public void deletePost(Long id)
+    {
+        Free post = getPostId(id);
+        freeRepository.delete(post);
+    }
+
+    public List<Free> getAllPosts(){
+        return freeRepository.findAll();
+    }
+
+    public Free getPostId(Long id){
+        return freeRepository.findById(id).orElseThrow(()-> new RuntimeException("게시글을 찾을 수 없습니다."));
+    }
 }
