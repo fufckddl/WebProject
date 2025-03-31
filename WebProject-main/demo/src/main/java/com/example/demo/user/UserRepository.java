fc3fe4bff6d143c24fd63a7874cbc9cd2f5c9b3f
Lee ChangRyoel <dlckdfuf141@naver.com>
2025-03-31 21:51:42 +0900

    Delete WebProject-main directory

--- WebProject-main/demo/src/main/java/com/example/demo/user/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<SiteUser, Long> {
-    SiteUser findByUsername(String username);
-    //= SELECT * FROM site_user WHERE username = "홍길동"
-}
